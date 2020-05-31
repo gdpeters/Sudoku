df0a97c3174ba4f88e7d8cf3d40122e9fbc2ccda
gdpeters <43419592+gdpeters@users.noreply.github.com>
2020-05-30 21:49:10 -0500

    added files

---
+++ Sudoku/Game.cs
@@ -0,0 +1,240 @@
+﻿/* 
+ * Genevieve Peters - Code Sample
+ * 
+ * The following sample is the Game class from a Suduko WPF application
+ * written in C# using the .NET environment in Visual Studio.
+ * 
+ * The Game class creates a valid Sudoku gameboard and adheres to the rules of Sudoku:
+ * - Valid numbers = 1,2,3,4,5,6,7,8,9
+ * - Valid numbers must only appear once in each row.
+ * - Valid numbers must only appear once in each column.
+ * - Valid numbers must only appear once in each 3x3 block starting in the top left corner of the board.
+ * - Omitted cells must be filled in by the player.
+ * - If the gameboard exactly matches the solution, the player wins.
+ * 
+ */
+
+using System;
+using System.Collections;
+
+namespace Sudoku
+{
+    class Game
+    {
+        private int[,] solution; //complete gameboard
+        private int[,] startBoard; //gameboard with omitted cells
+
+        public Game()
+        {
+            solution = new int[9, 9];
+            startBoard = new int[9, 9];
+        }
+
+        /// <summary>
+        /// Generates a valid Sudoku board
+        /// </summary>
+        /// <param name="level">the number of cells to omit</param>
+        public void Generate(int level)
+        {
+            int startRow = 0;
+            int startCol = 0;
+            bool filled = this.FillCell(startRow, startCol, this.GetOptions(startRow, startCol));
+            if (filled)
+                startBoard = this.HideCells(solution, level);
+        }
+
+        /// <summary>
+        /// Returns true if the gameboard is valid.
+        /// Recursive method that assigns random numbers from the list of
+        /// optional numbers. If assignment is not valid, disregard number
+        /// and choose another value from the options list. If list is empty,
+        /// backtrack to the previous cell, choose a new value and repeat.
+        /// </summary>
+        /// <param name="r">row</param>
+        /// <param name="c">column</param>
+        /// <param name="ops">options list containing valid numbers for cell[r,c]</param>
+        /// <returns>true if the gameboard is valid</returns>
+        private bool FillCell(int r, int c, ArrayList ops)
+        {
+            if (ops.Count < 1)
+                return false;
+            
+            Random rand = new Random();
+            int opsIndex = rand.Next(ops.Count);
+            int num = (int)ops[opsIndex];
+            solution[r, c] = num;
+            if (r == 8 && c == 8)
+                return true;
+            else
+            {
+                int nextR = GetNextRow(r, c);
+                int nextC = GetNextCol(r, c);
+                bool success = FillCell(nextR, nextC, this.GetOptions(nextR, nextC));
+
+                if (success)
+                    return true;
+                
+                ops.RemoveAt(opsIndex);
+                solution[r, c] = 0;
+                return FillCell(r, c, ops);
+            }
+        }
+
+        /// <summary>
+        /// Returns the row number of the next cell
+        /// following the current cell.
+        /// </summary>
+        /// <param name="r">row of current cell</param>
+        /// <param name="c">column of current cell</param>
+        /// <returns>row number of next cell</returns>
+        private int GetNextRow(int r, int c)
+        {
+            if (c == 8)
+                return r + 1;
+            else
+                return r;
+        }
+
+        /// <summary>
+        /// Returns the column number of the next cell
+        /// following the current cell.
+        /// </summary>
+        /// <param name="r">row of current cell</param>
+        /// <param name="c">column of current cell</param>
+        /// <returns>column number of next cell</returns>
+        private int GetNextCol(int r, int c)
+        {
+            if (c == 8)
+                return 0;
+            else
+                return c + 1;
+        }
+
+        /// <summary>
+        /// Returns a list of valid numbers for cell[row,col]
+        /// </summary>
+        /// <param name="r">row</param>
+        /// <param name="c">column</param>
+        /// <returns>list of valid numbers for cell[row,col]</returns>
+        private ArrayList GetOptions(int r, int c)
+        {
+            ArrayList lists = new ArrayList();
+            lists.AddRange(this.GetRow(r));
+            lists.AddRange(this.GetCol(c));
+            lists.AddRange(this.GetBlock(r, c));
+
+            ArrayList options = new ArrayList();
+            for (int d = 1; d < 10; d++)
+            {
+                if (!lists.Contains(d))
+                    options.Add(d);
+            }
+            return options;
+        }
+
+        /// <summary>
+        /// Returns a list of numbers currently in row r.
+        /// </summary>
+        /// <param name="r">row</param>
+        /// <returns>list of numbers currently in row r</returns>
+        private ArrayList GetRow(int r)
+        {
+            ArrayList row = new ArrayList();
+            for (int i = 0; i < 9; i++)
+            {
+                if (solution[r, i] != 0)
+                    row.Add(solution[r, i]);
+            }
+            return row;
+        }
+
+        /// <summary>
+        /// Returns a list of numbers currently in column c
+        /// </summary>
+        /// <param name="c">column</param>
+        /// <returns>list of numbers currently in column c</returns>
+        private ArrayList GetCol(int c)
+        {
+            ArrayList col = new ArrayList();
+            for (int i = 0; i < 9; i++)
+            {
+                if (solution[i, c] != 0)
+                    col.Add(solution[i, c]);
+            }
+            return col;
+        }
+
+        /// <summary>
+        /// Returns a list of numbers currently in the 3x3
+        /// block where cell[r,c] resides
+        /// </summary>
+        /// <param name="r">row</param>
+        /// <param name="c">column</param>
+        /// <returns>list of numbers currently in block</returns>
+        private ArrayList GetBlock(int r, int c)
+        {
+            int blockR = (r / 3) * 3;
+            int blockC = (c / 3) * 3;
+
+            ArrayList block = new ArrayList();
+            for (int i = blockR; i < blockR + 3; i++)
+            {
+                for (int j = blockC; j < blockC + 3; j++)
+                {
+                    block.Add(solution[i, j]);
+                }
+            }
+            return block;
+        }
+
+        /// <summary>
+        /// Returns a gameboard with omitted cells.
+        /// </summary>
+        /// <param name="fullBoard">solution for this game</param>
+        /// <param name="level">number of cells to omit</param>
+        /// <returns>gameboard with omitted cells</returns>
+        private int[,] HideCells(int[,] fullBoard, int level)
+        {
+            int[,] modified = (int[,])fullBoard.Clone();
+            ArrayList hideIndex = new ArrayList();
+            Random rand = new Random();
+            int i = 0;
+            while (i < level)
+            {
+                int cellIndex = rand.Next(81);//0-80
+                if (!hideIndex.Contains(cellIndex))
+                {
+                    hideIndex.Add(cellIndex);
+                    i++;
+                }
+            }
+
+            foreach (int cell in hideIndex)
+            {
+                int row = cell / 9;
+                int col = cell % 9;
+                modified[row, col] = 0;
+            }
+
+            return modified;
+        }
+
+        public int[,] GetStartBoard()
+        {
+            return startBoard;
+        }
+
+        public bool IsSolved(int[,] currentBoard)
+        {
+            for (int r = 0; r < 9; r++)
+            {
+                for (int c = 0; c < 9; c++)
+                {
+                    if (solution[r, c] != currentBoard[r, c])
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Sudoku/MainWindow.xaml.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+
+namespace Sudoku
+{
+    public partial class MainWindow : Window
+    {
+        private const int EASY = 30;
+        private const int MED = 40;
+        private const int HARD = 50;
+        private Game game;
+        private int[,] currentBoard;
+        private DispatcherTimer t, fader;
+        private DateTime startTime, pausedTime;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            menuPopup.IsOpen = true;
+        }
+
+        private void NewGameClick(object sender, RoutedEventArgs e)
+        {
+            if (t != null)
+                t.Stop();
+            game = new Game();
+            menuPopup.IsOpen = false;
+            levelPopup.IsOpen = true;
+        }
+
+        private void EasyClick(object sender, RoutedEventArgs e)
+        {
+            this.ChooseLevel(EASY);
+        }
+        private void MediumClick(object sender, RoutedEventArgs e)
+        {
+            this.ChooseLevel(MED);
+        }
+        private void HardClick(object sender, RoutedEventArgs e)
+        {
+            this.ChooseLevel(HARD);
+        }
+        private void ChooseLevel(int level)
+        {
+            game.Generate(level);
+            this.WriteCells();
+            levelPopup.IsOpen = false;
+            this.SetTimer();
+        }
+
+        private void SetTimer()
+        {
+            t = new DispatcherTimer();
+            t.Interval = TimeSpan.FromSeconds(1);
+            startTime = DateTime.Now;
+            t.Tick += UpdateClock;
+            t.Start();
+        }
+
+        private void UpdateClock(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = DateTime.Now - startTime;
+            int hh = elapsed.Hours;
+            int mm = elapsed.Minutes;
+            int ss = elapsed.Seconds;
+            Clock.Text = String.Format("{0,2:D2}:{1,2:D2}:{2,2:D2}", hh, mm, ss);
+        }
+
+        private void WriteCells()
+        {
+            int[,] board = game.GetStartBoard();
+            currentBoard = new int[9, 9];
+            for (int r = 0; r < 9; r++)
+            {
+                for (int c = 0; c < 9; c++)
+                {
+                    string cellName = "cell" + r.ToString() + c.ToString();
+                    TextBox txtbx = (TextBox)this.FindName(cellName);
+                    if (board[r, c] == 0)
+                    {
+                        txtbx.Text = "";
+                        txtbx.IsReadOnly = false;
+                        txtbx.MaxLength = 1;
+                        txtbx.MaxLines = 1;
+                        txtbx.Foreground = new SolidColorBrush(Colors.Blue);
+                    }
+                    else
+                    {
+                        txtbx.Text = board[r, c].ToString();
+                        txtbx.IsReadOnly = true;
+                        txtbx.Foreground = new SolidColorBrush(Colors.Black);
+                    }
+                    currentBoard[r, c] = board[r, c];
+                    txtbx.TextChanged += new TextChangedEventHandler(Text_Changed);
+                }
+            }
+        }
+
+        private void RestartClick(object sender, RoutedEventArgs e)
+        {
+            t.Stop();
+            this.WriteCells();
+            menuPopup.IsOpen = false;
+            this.SetTimer();
+        }
+
+        private void QuitClick(object sender, RoutedEventArgs e)
+        {
+            t.Stop();
+            menuPopup.IsOpen = false;
+            this.Close();
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            menuPopup.IsOpen = false;
+            t.Start();
+            startTime = startTime + (DateTime.Now - pausedTime);
+        }
+
+        private void OpenMenu(object sender, RoutedEventArgs e)
+        {
+            t.Stop();
+            pausedTime = DateTime.Now;
+            menuPopup.IsOpen = true;
+        }
+
+        private void Text_Changed(object sender, TextChangedEventArgs e)
+        {
+            TextBox txtbx = (TextBox)sender;
+            string txtbxName = txtbx.Name;
+            int r = Int32.Parse(txtbxName.ToCharArray()[4].ToString());
+            int c = Int32.Parse(txtbxName.ToCharArray()[5].ToString());
+            try
+            {
+                currentBoard[r, c] = Int32.Parse(txtbx.Text);
+                this.CheckSolution();
+            }
+            catch (System.FormatException ex) { }
+        }
+
+        private void CheckSolution()
+        {
+            bool isReady = true;
+            for (int r = 0; r < 9; r++)
+            {
+                for (int c = 0; c < 9; c++)
+                {
+                    if (currentBoard[r,c] == 0)
+                    {
+                        isReady = false;
+                    }
+                }
+            }
+
+            if (isReady)
+            {
+                t.Stop();
+                fader = new DispatcherTimer();
+                fader.Interval = TimeSpan.FromMilliseconds(75);
+
+                if (game.IsSolved(currentBoard))
+                {
+                    solved.Visibility = Visibility.Visible;                    
+                }
+                else
+                {
+                    notSolved.Visibility = Visibility.Visible;
+                    t.Start();
+                }
+                fader.Tick += RemoveBanner;
+                fader.Start();
+
+            }
+        }
+
+        private void RemoveBanner(object sender, EventArgs e)
+        {
+            TextBlock banner;
+
+            if (solved.Visibility == Visibility.Visible)
+                banner = solved;
+            else
+                banner = notSolved;
+
+            if (banner.FontSize < 45)
+            {
+                fader.Stop();
+                banner.Visibility = Visibility.Hidden;
+                banner.FontSize = 68;
+                banner.Opacity = 100;
+            }
+            else
+            {
+                banner.FontSize = banner.FontSize - 0.5;
+                banner.Opacity = banner.Opacity * 0.95;
+            }
+        }
+    }
+}
